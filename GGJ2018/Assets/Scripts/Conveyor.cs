5f2a5453d178ebef4f2f4dd29733dec2af4a9932
Tianyi Zhao <tz1@andrew.cmu.edu>
2018-01-27 13:06:33 -0500

    some package modify and init conveyor

---
+++ GGJ2018/Assets/Scripts/Conveyor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conveyor : MonoBehaviour {
+
+    public float Speed;
+    public bool Direction; // true: forward
+
+    [SerializeField] Transform StartGearTransform, EndGearTransform;
+    [SerializeField] Vector3 SpawnOffset;
+
+    [SerializeField] GameObject SpawnUnit;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
