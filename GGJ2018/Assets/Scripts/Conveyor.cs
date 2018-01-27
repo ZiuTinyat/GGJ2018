be5cae6747f0eff6d391df084fb90618fa8a6e8c
Ruili Tang <ruilit@andrew.ad.cmu.edu>
2018-01-27 15:14:34 -0500

    try physics - from Tianyi

--- GGJ2018/Assets/Scripts/Conveyor.cs
+++ GGJ2018/Assets/Scripts/Conveyor.cs
@@ -4,21 +4,47 @@ using UnityEngine;
 
 public class Conveyor : MonoBehaviour {
 
+    // Real-time dragging not supported yet!
+
     public float Speed;
     public bool Direction; // true: forward
 
     [SerializeField] Transform StartGearTransform, EndGearTransform;
-    [SerializeField] Vector3 SpawnOffset;
 
+    private List<ConveyorUnit> ConveyorUnits = new List<ConveyorUnit>();
+
+    [SerializeField] Transform UnitContainerTransform;
     [SerializeField] GameObject SpawnUnit;
 
 	// Use this for initialization
 	void Start () {
-		
+        StartCoroutine(ConveyorUnitProcessCoroutine());
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (ConveyorUnits.Count > 0 && ConveyorUnits[ConveyorUnits.Count - 1].x > 0.2f)
+        {
+            StartCoroutine(ConveyorUnitProcessCoroutine());
+        }
 	}
+
+    IEnumerator ConveyorUnitProcessCoroutine()
+    {
+        GameObject unitObj = Instantiate(SpawnUnit, StartGearTransform.position, Quaternion.identity, UnitContainerTransform);
+        ConveyorUnit unit = unitObj.GetComponent<ConveyorUnit>();
+        ConveyorUnits.Add(unit);
+        do
+        {
+            yield return null;
+            unit.totX = Vector3.Distance(EndGearTransform.position, StartGearTransform.position);
+            unit.x += Speed * Time.deltaTime;
+            //unitObj.transform.position = Vector3.Lerp(StartGearTransform.position, EndGearTransform.position, unit.percent);
+            //Vector3 dir= (EndGearTransform.position - StartGearTransform.position).normalized;
+            unit.rigidbody.MovePosition(Vector3.Lerp(StartGearTransform.position, EndGearTransform.position, unit.percent));
+        }
+        while (unit.percent < 1f);
+        ConveyorUnits.Remove(unit);
+        Destroy(unitObj);
+    }
 }
---
+++ GGJ2018/Assets/Scripts/ConveyorUnit.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorUnit : MonoBehaviour {
+    public float x = 0f;
+    public float totX = 0f;
+    public float percent { get { return x / totX; } }
+    public new Rigidbody rigidbody { get { return GetComponent<Rigidbody>(); } }
+}
