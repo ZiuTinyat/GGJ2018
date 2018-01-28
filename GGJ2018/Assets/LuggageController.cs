52b2cbbbf9f837bc5206aed509776b237d60a8d1
XuejunWang <xuejunw1@andrew.cmu.edu>
2018-01-28 11:59:33 -0500

    crack sound

--- GGJ2018/Assets/LuggageController.cs
+++ GGJ2018/Assets/LuggageController.cs
@@ -10,18 +10,21 @@ public class LuggageController : MonoBehaviour {
     public bool InHand;
     [SerializeField]
     private GameObject originalLuggage;
-
     [SerializeField]
     private GameObject brokenLuggage;
+    private AudioSource m_crackSoundSource;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
         m_rigidbody = GetComponent<Rigidbody>();
         brokenLuggage.SetActive(false);
-	}
+        m_crackSoundSource = GetComponent<AudioSource>();
+    }
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
 
@@ -32,12 +35,16 @@ public class LuggageController : MonoBehaviour {
         brokenLuggage.SetActive(true);
         brokenLuggage.transform.position = originalLuggage.transform.position;
         brokenLuggage.transform.rotation = originalLuggage.transform.rotation;
+        if (!m_crackSoundSource.isPlaying)
+        {
+            m_crackSoundSource.Play();
+        }
         Destroy(originalLuggage);
         Invoke("DestroyObject", 10f);
     } 
 
-    private void DestroyObject() {
+    private void DestroyObject()
+    {
         Destroy(gameObject);
     }
-
 }
