c61372cf850725ffd69ff48cd6c3220fbed477f2
Tianyi Zhao <tz1@andrew.cmu.edu>
2018-01-28 01:17:42 -0500

    Scoring and UI

---
+++ GGJ2018/Assets/Scripts/GameController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+    public static GameController Instance { get; private set; }
+    public static int Score { get; private set; }
+    private static int ScoreStartSize;
+
+    [SerializeField] Text ScoreText;
+    
+    public static void AddScore(int add) {
+        Score += add;
+        Instance.ScoreText.text = Score.ToString();
+        Instance.StopCoroutine(ScoreEffect());
+        Instance.StartCoroutine(ScoreEffect());
+    }
+
+    static IEnumerator ScoreEffect() {
+        int increase = 40;
+        while (increase > 0) {
+            Instance.ScoreText.fontSize = ScoreStartSize + increase;
+            increase--;
+            yield return null;
+        }
+    }
+
+    private void Awake() {
+        Instance = this;
+    }
+
+    private void Start() {
+        ScoreStartSize = Instance.ScoreText.fontSize;
+    }
+}
---
+++ GGJ2018/Assets/Scripts/Luggage.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Luggage : MonoBehaviour {
+
+    public static string Tag { get { return "Luggage"; } }
+    public static Dictionary<LuggageType, int> ValueTable;
+
+    public enum LuggageType {
+        Default, 
+        Common, 
+        Valuable, 
+        Luxury
+    }
+
+    public LuggageType Type;
+
+    private void OnTriggerEnter(Collider other) {
+        
+    }
+
+    private void Awake() {
+        if (ValueTable == null) {
+            ValueTable = new Dictionary<LuggageType, int>();
+            ValueTable.Add(LuggageType.Default, 5);
+            ValueTable.Add(LuggageType.Common, 5);
+            ValueTable.Add(LuggageType.Valuable, 10);
+            ValueTable.Add(LuggageType.Luxury, 25);
+        }
+    }
+}
