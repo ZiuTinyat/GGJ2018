844eb163c9ef964998beba99c190451c72ef01f6
XuejunWang <xuejunw1@andrew.cmu.edu>
2018-01-28 11:53:18 -0500

    added success sound

--- GGJ2018/Assets/Scripts/GoalController.cs
+++ GGJ2018/Assets/Scripts/GoalController.cs
@@ -4,9 +4,20 @@ using UnityEngine;
 
 public class GoalController : MonoBehaviour {
 
+    private AudioSource m_successSoundSource;
+
+    private void Start()
+    {
+        m_successSoundSource = GetComponent<AudioSource>();
+    }
+
     private void OnTriggerEnter(Collider other) {
         if (other.tag == Luggage.Tag) {
             GameController.AddScore(Luggage.ValueTable[other.GetComponent<Luggage>().Type]);
+            if (!m_successSoundSource.isPlaying)
+            {
+                m_successSoundSource.Play();
+            }
         }
     }
 
