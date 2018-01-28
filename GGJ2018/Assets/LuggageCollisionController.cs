cc217a7cc786e5cbe513b2e0bc8a32adcbb62b9a
XuejunWang <xuejunw1@andrew.cmu.edu>
2018-01-28 01:09:18 -0500

    Add broken suitcase model Add complete suitcase prefabs

---
+++ GGJ2018/Assets/LuggageCollisionController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class LuggageCollisionController : MonoBehaviour {
+
+    UnityEvent bagCollision;
+    public LuggageController LuggageController;
+
+	// Use this for initialization
+	void Start () {
+        LuggageController = GetComponentInParent<LuggageController>();
+        if (bagCollision == null)
+        {
+            bagCollision = new UnityEvent();
+        }
+        bagCollision.AddListener(LuggageController.Crush);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground") && !LuggageController.Crushed)
+        {
+            bagCollision.Invoke();
+        }
+    }
+}
---
+++ GGJ2018/Assets/LuggageController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LuggageController : MonoBehaviour {
+
+    private Rigidbody m_rigidbody;
+    public bool Crushed;
+
+    [SerializeField]
+    private GameObject originalLuggage;
+
+    [SerializeField]
+    private GameObject brokenLuggage;
+
+	// Use this for initialization
+	void Start () {
+        m_rigidbody = GetComponent<Rigidbody>();
+        brokenLuggage.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Crush()
+    {
+        Crushed = true;
+        Debug.Log("Box Crushed");
+        Destroy(originalLuggage);
+        brokenLuggage.SetActive(true);
+    } 
+
+}
