fe65c64ea1a32996b8dc37bcd9b07038a5b484c6
Tianyi Zhao <tz1@andrew.cmu.edu>
2018-01-28 04:54:09 -0500

    start page

---
+++ GGJ2018/Assets/Scripts/StartUIController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartUIController : MonoBehaviour {
+
+    [SerializeField] SpriteRenderer FaderSprite, CreditSprite, StartSprite, ButtonSprite;
+
+    private Color TransparentColor = new Color(1f, 1f, 1f, 0f);
+
+	// Use this for initialization
+	void Start () {
+        CreditSprite.color = TransparentColor;
+        StartSprite.color = TransparentColor;
+        ButtonSprite.color = TransparentColor;
+        StartCoroutine(StartUICoroutine());
+	}
+
+    IEnumerator StartUICoroutine() {
+        yield return new WaitForSeconds(1f);
+
+        // Show credit sprite
+        // TODO: Audio
+        yield return StartCoroutine(FadeSprite(CreditSprite, 1f, Color.white));
+        yield return new WaitForSeconds(1f);
+        yield return StartCoroutine(FadeSprite(CreditSprite, 1f, TransparentColor));
+        yield return new WaitForSeconds(1f);
+
+        // Show start page
+        yield return StartCoroutine(FadeSprite(StartSprite, 1f, Color.white));
+        yield return StartCoroutine(FadeSprite(ButtonSprite, 1f, Color.white));
+
+        // Wait for click
+        yield return new WaitUntil(() => (Input.anyKeyDown || Input.GetMouseButtonDown(0)));
+
+        // Start Game
+        StartCoroutine(FadeSprite(StartSprite, 1f, TransparentColor));
+        yield return StartCoroutine(FadeSprite(ButtonSprite, 1f, TransparentColor));
+        UnityEngine.SceneManagement.SceneManager.LoadScene("main");
+    }
+
+    IEnumerator FadeSprite(SpriteRenderer sprite, float duration, Color end) {
+        Color start = sprite.color;
+        float t = 0f;
+        while (t < duration) {
+            sprite.color = Color.Lerp(start, end, t / duration);
+            t += Time.deltaTime;
+            yield return null;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
